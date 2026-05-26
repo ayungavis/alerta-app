2fe72dcdb25ec9456cb57e4229168cc6dd43b60c
Wahyu Kurniawan <ayungavis@gmail.com>
2026-05-26 13:33:35 +0800

    Initial Commit

---
+++ AlertaApp/AlertaAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  AlertaAppApp.swift
+//  AlertaApp
+//
+//  Created by Wahyu Kurniawan on 26/05/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct AlertaAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
